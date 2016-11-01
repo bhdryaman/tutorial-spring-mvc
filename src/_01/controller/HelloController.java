9bb192aa6371cdb050604d1e946103e7e55c937a
bhdryaman <bhdr.yaman@gmail.com>
2016-11-01 15:02:53 +0200

    initial commit...

---
+++ src/_01/controller/HelloController.java
@@ -0,0 +1,31 @@
+package _01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/helloSpringMVC", method = RequestMethod.GET)
+	public String handleRequest(ModelMap model) {
+		System.out.println("...handleRequest...");
+		String message = "Hello from handleRequest()";
+		model.addAttribute("msg", message);
+		return "_01/hello";
+	}
+	
+	@RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "_01/hello";
+    }
+    
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String index(ModelMap model) {
+        model.addAttribute("greeting", "index In HelloController");
+        return "_01/hello";
+    }
+
+}
